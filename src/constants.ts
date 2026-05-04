97400055bdd9dee5db758748fa2db18b77919077
Ashish Sapkota <ashishspkt6566@gmail.com>
2026-05-04 16:01:12 +0545

    chore: format changes in constants.ts and error.ts

--- src/constants.ts
+++ src/constants.ts
@@ -1,6 +1,5 @@
-import { BigNumber } from "bignumber.js";
-
 import { utils, web3 } from "@coral-xyz/anchor";
+import { BigNumber } from "bignumber.js";
 
 // Transaction timing constants (in milliseconds)
 export const DEFAULT_SEND_TRANSACTION_INTERVAL = 1000;
@@ -19,9 +18,13 @@ export const SOL_DECIMALS = 9;
 /**
  * WSOL Mint Address
  */
-export const WSOL = new web3.PublicKey("So11111111111111111111111111111111111111112");
+export const WSOL = new web3.PublicKey(
+	"So11111111111111111111111111111111111111112",
+);
 
-export const ZBCN = new web3.PublicKey("ZBCNpuD7YMXzTHB2fhGkGi78MNsHGLRXUhRewNRm9RU");
+export const ZBCN = new web3.PublicKey(
+	"ZBCNpuD7YMXzTHB2fhGkGi78MNsHGLRXUhRewNRm9RU",
+);
 
 /** BigNumber Object for 10 */
 export const TEN_BIGNUM = BigNumber(10);
@@ -35,7 +38,9 @@ export const TOKEN_PROGRAM_ID = utils.token.TOKEN_PROGRAM_ID;
 export const ASSOCIATED_TOKEN_PROGRAM_ID = utils.token.ASSOCIATED_PROGRAM_ID;
 
 /** Memo Program ID */
-export const MEMO_PROGRAM_ID = new web3.PublicKey("MemoSq4gqABAXKb96qnH8TysNcWxMyWCqXgDLGmfcHr");
+export const MEMO_PROGRAM_ID = new web3.PublicKey(
+	"MemoSq4gqABAXKb96qnH8TysNcWxMyWCqXgDLGmfcHr",
+);
 
 /** Compute units for the Compute Budget Program */
 export const COMPUTE_BUDGET_PROGRAM_COMPUTE_UNIT = 400;
--- src/error.ts
+++ src/error.ts
@@ -2,7 +2,10 @@ export class MultiTransactionSimulationError extends Error {
 	name: string = "MultiTransactionSimulationError";
 	simulationErrors: { index: number; error: any }[];
 
-	constructor(message: string, simulationErrors: { index: number; error: any }[]) {
+	constructor(
+		message: string,
+		simulationErrors: { index: number; error: any }[],
+	) {
 		super(message);
 		this.simulationErrors = simulationErrors;
 	}
